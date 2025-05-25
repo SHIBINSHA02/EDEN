be819a5f4b520ee0a8ece018c80cc52e4bd28f29
SHIBINSHA <shibin24888@gmail.com>
2025-05-25 19:09:30 +0530

    see_shedule_button added

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,23 +1,33 @@
-import PixelArtBackground from "../Background/PixelArtbg";
-import "./About.css";
+import React from 'react';
+import PixelArtBackground from '../Background/PixelArtbg';
+import './About.css';
 
 export const About = ({ description, buttonText }) => {
   const handleClick = () => {
     window.location.href = "https://www.youtube.com/";
   };
+
+  const handleDownload = () => {
+    const pdfUrl = "./MODULE.pdf"; // Assumes schedule.pdf is in the public directory
+    const link = document.createElement('a');
+    link.href = pdfUrl;
+    link.download = 'MODULE.pdf'; // Corrected file name for download
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <div className="about-container">
-      <PixelArtBackground
-        className="about-background"
-        pixelSize={2}
-        density={1}
-        fadeDuration={3000}
-      />
+      <PixelArtBackground className="about-background" pixelSize={2} density={1} fadeDuration={3000} />
       <div className="about-content">
         <div className="description">
           <p>{description}</p>
         </div>
-        <button onClick={handleClick}>{buttonText}</button>
+        <div className="button-container">
+          <button className="primary-button" onClick={handleClick}>{buttonText}</button>
+          <button className="secondary-button" onClick={handleDownload}>See Schedule</button>
+        </div>
       </div>
     </div>
   );
