b33a92f6fc6246fb5de4015abc7bc78c919c4ff5
SHIBINSHA <shibin24888@gmail.com>
2025-05-27 17:43:31 +0530

    speed adjust

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -14,7 +14,7 @@ export const About = ({ description, buttonText }) => {
       const timeout = setTimeout(() => {
         setDisplayText((prevText) => prevText + description[currentIndex])
         setCurrentIndex((prevIndex) => prevIndex + 1)
-      }, 2) // Faster typing speed
+      }, 5) // Faster typing speed
       return () => clearTimeout(timeout)
     }
   }, [currentIndex, description])
